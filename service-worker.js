a549eecf8e68e7202f3ecefb442ef1e647300a5a
minmin435 <jungmk0921@gmail.com>
2025-08-12 22:11:26 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("tamagotchi-cache").then(cache => {
+      return cache.addAll([
+        "/tamagotchi-pwa.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
